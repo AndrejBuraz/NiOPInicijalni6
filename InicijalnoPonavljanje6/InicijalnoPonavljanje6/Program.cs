9253260b2ab8d2a1f45c0bcb14aaca2a7633bdd5
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-14 19:53:49 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje6/InicijalnoPonavljanje6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Napišite program koji će izračunati sumu reda:
+                1/3 + 1/9 + 1/27 + . . .
+                Korisnik treba unijeti broj članova reda.
+             */
+            Console.WriteLine("Unesi broj članova reda: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double suma = 0, stupanj = 3.0;
+            for (int i = 0; i < n; i++)
+            {
+                suma += (1 / stupanj);
+                stupanj *= 3;
+            }
+            Console.WriteLine("Suma: " + suma);
+            Console.ReadKey();
+        }
+    }
+}
